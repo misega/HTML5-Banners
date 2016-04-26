3788a90b130d25967487c28d5350948d4c12f6dc
Michael Galloway <michaelgalloway_35@hotmail.com>
2016-04-26 09:30:35 -0500

    Add PostCSS to compile styles

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,32 @@ var gulp = require('gulp-help')(require('gulp'), {'description': '', 'hideEmpty'
 var pkg = require('./package.json');
 var browserSync = require('browser-sync');
 var sequence = require('run-sequence').use(gulp);
-
+// POSTCSS Packages
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var variables = require('postcss-advanced-variables');
+var cachebuster = require('postcss-cachebuster');
+var calc = require('postcss-calc');
+var position = require('postcss-position-alt');
+var sorting = require('postcss-sorting');
 // Misc NPM Packages
 var del = require('del');
+var git = require('gulp-git');
 var fs = require('fs-jetpack');
 var size = require('gulp-size');
 var gutil = require('gulp-util');
 var notify = require('gulp-notify');
+var rename = require('gulp-rename');
 var plumber = require('gulp-plumber');
 var flags = require('minimist')(process.argv.slice(2));
-var Supplant = require('supplant');
-var subs = new Supplant();
 
 var devFolder = '';
+var browserSyncRewriteRules = [{
+    match: /<!-- {inject:banner-controls} -->/ig,
+    fn: function(match) {
+        return '<script src="assets/_dev-build/_banners.js"></script>';
+    }
+}];
 
 /* Setup: Initial values from banner boilerplate
 --------------------------------------------------------------------------- */
@@ -26,6 +39,18 @@ var defaults = {
     title: '{{Project Title}}', // Will be used on the review page template
     desc: '{{Project Description}}', // Will be used on the review page template
     folder: '_banner-template' // starter boilerplate to build banners
+};
+
+/* Setup: File paths
+--------------------------------------------------------------------------- */
+var paths = {
+    html: '/index.html',
+    img: '/assets/img/**/*',
+    css: {
+        source: '/assets/css/source-style.css',
+        destination: '/assets/css/'
+    },
+    js: '/assets/js/*.js'
 };
 
 /* Utilities
@@ -66,18 +91,18 @@ gulp.task('deploy', 'zip up banner directories for distribution', function(done)
 
 /* Watch a specific banner directory; use --flag to declare folder name
 --------------------------------------------------------------------------- */
-gulp.task('watch', 'monitor files for changes', ['preflight-directory'], function(done) {
-    // gulp.watch([config.scss.watchFiles]).on('change', function() {
-    //     sequence('styles', browserSync.reload);
+gulp.task('watch', 'monitor files for changes', ['preflight-directory', 'styles', 'browserSync'], function(done) {
+    gulp.watch([devFolder + paths.html]).on('change', function() {
+        sequence('html', browserSync.reload);
+    });
+    // gulp.watch([devFolder + paths.img]).on('change', function() {
+    //     sequence('assets', browserSync.reload);
     // });
-    // gulp.watch([config.scss.copyFiles]).on('change', function() {
-    //     sequence('copy:styles', browserSync.reload);
-    // });
-    // gulp.watch(config.js.watchFiles).on('change', function() {
+    gulp.watch(devFolder + paths.css.source).on('change', function() {
+        sequence('styles', browserSync.reload);
+    });
+    // gulp.watch(devFolder + paths.js).on('change', function() {
     //     sequence('scripts', browserSync.reload);
-    // });
-    // gulp.watch(config.js.copyFiles).on('change', function() {
-    //     sequence('copy:scripts', browserSync.reload);
     // });
 }, {
     options: {
@@ -85,25 +110,78 @@ gulp.task('watch', 'monitor files for changes', ['preflight-directory'], functio
         'controls': 'show banner controls; enables scrubbing through timeline'
     }
 });
-// --300x250
-// --controls
 
+gulp.task('html', false, function(done) {
+    return gulp
+        .src(devFolder + paths.html)
+        .pipe(plumber({ errorHandler: reportError }));
+});
 
-/* SUB-TASKS
-==================================================================================================== */
+// gulp.task('assets', false, function() {
+//     return gulp
+//         .src(devFolder + paths.img);
+// });
+
+gulp.task('styles', false, function() {
+    var processors = [
+        autoprefixer({ browsers: ['> 1%', 'last 3 versions', 'ie >= 9', 'ios >= 7', 'android >= 4.4']}),
+        variables(),
+        position(),
+        calc()
+        //sorting({ 'sort-order': 'zen' })
+    ];
+
+    return gulp
+        .src(devFolder + paths.css.source)
+        .pipe(plumber({ errorHandler: reportError }))
+        .pipe(postcss(processors))
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest(devFolder + paths.css.destination));
+});
+
+// gulp.task('scripts', false, function() {
+//     return gulp
+//         .src(devFolder + paths.script);
+// });
 
 /* Watch a specific banner directory; use --flag to declare folder name
 --------------------------------------------------------------------------- */
-gulp.task('browserSync', false, function(done) {
+gulp.task('browserSync', false, function() {
     browserSync({
-        //proxy: 'public.drupal7.broco',
         server: { baseDir: devFolder },
         open: true,
         notify: false,
-        // See: System Preferences… » Sound » Sound Effects
-        sound: 'Basso'
+        rewriteRules: (flags.controls)? browserSyncRewriteRules : []
     });
 });
+
+/* SUB-TASKS
+==================================================================================================== */
+var reportError = function(error) {
+    var lineNumber = (error.line) ? 'LINE ' + error.line + ' -- ' : '';
+
+    notify({
+        title: 'Task Failed [' + error.plugin + ']',
+        message: lineNumber + 'See console.',
+        // See: System Preferences… » Sound » Sound Effects
+        sound: 'Basso'
+    }).write(error);
+
+    // Pretty error reporting
+    var report = '';
+    var chalk = gutil.colors.white.bgRed;
+
+    report += chalk('TASK:') + ' [' + error.plugin + ']\n';
+    if (error.line) { report += chalk('LINE:') + ' ' + error.line + '\n'; }
+    report += chalk('PROB:') + ' ' + error.messageFormatted + '\n';
+    console.error(report);
+
+    // Prevent the 'watch' task from stopping
+    this.emit('end');
+};
+
+/* SUB-TASKS: Preflight Checklist
+==================================================================================================== */
 
 /* Setup: Make sure Package.json project info has been updated
 --------------------------------------------------------------------------- */
@@ -166,9 +244,10 @@ gulp.task('preflight-directory', false, function() {
         process.exit(1);
     }
 
-    // Directory Exists. Let's update the index page and style page to show the correct dimensions
+    /* Directory Exists. Let's update the index page and style page to show the correct dimensions
+    --------------------------------------------------------------------------- */
     var currentDirectory = fs.cwd('banners/' + flags.folder);
-
+    devFolder = currentDirectory.cwd();
     // The directory is available, let's update files to match the directory name
     var dimensions = flags.folder.match(/(\d{2,}x\d{2,})/g)[0].split('x');
     var size = {
@@ -176,14 +255,17 @@ gulp.task('preflight-directory', false, function() {
         height: dimensions[1]
     };
 
-    // set <meta name="ad.size"> and <title> to dimensions of folder
+    // index.html -- set <meta name="ad.size"> and <title> to dimensions of folder
     var adSizeRegExp = new RegExp('content="width=({{width}}|\d{2,}), height=({{height}}|\d{2,})"', 'g');
     var titleRexExp = new RegExp('<title>(.*)<\/title>', 'g');
-    var indexContent = currentDirectory.read('index.html');
-    indexContent = indexContent.toString().replace(adSizeRegExp, 'content="width=' + size.width + ', height=' + size.height + '"');
-    indexContent = indexContent.toString().replace(titleRexExp, '<title>Ad Banner: ' + size.width + 'x' + size.height + '</title>');
+    var indexContent = currentDirectory.read('index.html').toString();
+    indexContent = indexContent.replace(adSizeRegExp, 'content="width=' + size.width + ', height=' + size.height + '"');
+    indexContent = indexContent.replace(titleRexExp, '<title>Ad Banner: ' + size.width + 'x' + size.height + '</title>');
     fs.write(currentDirectory.cwd() + '/index.html', indexContent);
 
-    // set base folder for BrowserSync
-    devFolder = currentDirectory.cwd();
+    // source-style.css -- set css variable width/height
+    var styleContent = currentDirectory.read('assets/css/source-style.css').toString();
+    styleContent = styleContent.replace(/{{width}}/g, size.width + 'px');
+    styleContent = styleContent.replace(/{{height}}/g, size.height + 'px');
+    fs.write(currentDirectory.cwd() + '/assets/css/source-style.css', styleContent);
 });
